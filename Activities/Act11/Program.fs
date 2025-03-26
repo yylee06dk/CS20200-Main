9bf65c7bbcb052d66a9c50c7e6aa03fae80b575a
Sang Kil Cha <sangkilc@kaist.ac.kr>
2025-03-26 13:16:02 +0900

    Swap Act11 and Act12

--- Activities/Act11/Program.fs
+++ Activities/Act11/Program.fs
@@ -2,16 +2,47 @@
 
 module CS220.Program
 
-/// Fix this function to make `list1` and `list2` the same.
-let convert lst =
-  []
+/// Fix this function to make an imperative counter.
+let counter () =
+  42
+
+/// Fix this function to create a counter function.
+let createCounter () =
+  fun () -> 42
+
+type CounterState = {
+  Count: int
+}
+
+let createPureCounter () =
+  fun st -> { st with Count = st.Count + 1 }
 
 /// This is the main entry point.
 [<EntryPoint>]
 let main _ =
-  let list1 = List.init 1000 id
-  let list2 =
-    List.init 1000 (fun i -> if i % 2 = 0 then EvenNumber.create i
-                             else OddNumber.create i)
-  list1 = convert list2 |> printfn "%b"
+  (* mutable counter *)
+  let x = createCounter ()
+  let y = createCounter ()
+  let z = y
+  x () |> printfn "x: %d"
+  y () |> printfn "y: %d"
+  z () |> printfn "z: %d"
+  x () |> printfn "x: %d"
+  y () |> printfn "y: %d"
+  z () |> printfn "z: %d"
+  y () |> printfn "y: %d"
+  (* immutable counter *)
+  let x = createPureCounter ()
+  let y = createPureCounter ()
+  let stateX = { Count = 1 }
+  let stateY = { Count = 1 }
+  let stateZ = stateY
+  printfn $"x: {stateX.Count}"
+  printfn $"y: {stateY.Count}"
+  printfn $"z: {stateZ.Count}"
+  let stateX = x stateX
+  let stateY = y stateY
+  printfn $"x: {stateX.Count}"
+  printfn $"y: {stateY.Count}"
+  printfn $"z: {stateZ.Count}"
   0 (* Never modify this *)
--- Activities/Act12/Program.fs
+++ Activities/Act12/Program.fs
@@ -2,47 +2,16 @@
 
 module CS220.Program
 
-/// Fix this function to make an imperative counter.
-let counter () =
-  42
-
-/// Fix this function to create a counter function.
-let createCounter () =
-  fun () -> 42
-
-type CounterState = {
-  Count: int
-}
-
-let createPureCounter () =
-  fun st -> { st with Count = st.Count + 1 }
+/// Fix this function to make `list1` and `list2` the same.
+let convert lst =
+  []
 
 /// This is the main entry point.
 [<EntryPoint>]
 let main _ =
-  (* mutable counter *)
-  let x = createCounter ()
-  let y = createCounter ()
-  let z = y
-  x () |> printfn "x: %d"
-  y () |> printfn "y: %d"
-  z () |> printfn "z: %d"
-  x () |> printfn "x: %d"
-  y () |> printfn "y: %d"
-  z () |> printfn "z: %d"
-  y () |> printfn "y: %d"
-  (* immutable counter *)
-  let x = createPureCounter ()
-  let y = createPureCounter ()
-  let stateX = { Count = 1 }
-  let stateY = { Count = 1 }
-  let stateZ = stateY
-  printfn $"x: {stateX.Count}"
-  printfn $"y: {stateY.Count}"
-  printfn $"z: {stateZ.Count}"
-  let stateX = x stateX
-  let stateY = y stateY
-  printfn $"x: {stateX.Count}"
-  printfn $"y: {stateY.Count}"
-  printfn $"z: {stateZ.Count}"
+  let list1 = List.init 1000 id
+  let list2 =
+    List.init 1000 (fun i -> if i % 2 = 0 then EvenNumber.create i
+                             else OddNumber.create i)
+  list1 = convert list2 |> printfn "%b"
   0 (* Never modify this *)
