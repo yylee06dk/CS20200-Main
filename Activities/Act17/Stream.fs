58c91361ea3b2a2e1e55ac32de89e31e5bf8929b
yylee <yylee06dk@gmail.com>
2025-05-01 09:41:57 +0900

    Complete act 17

--- Activities/Act17/Stream.fs
+++ Activities/Act17/Stream.fs
@@ -7,11 +7,25 @@ type Stream<'a> =
   | Cons of 'a * (unit -> Stream<'a>)
 
 module Stream =
-  let car stream = failwith "Implement"
+  let car stream =
+    match stream with
+    | Cons (hd, tl) -> hd
+    | Nil -> failwith "Empty"
 
-  let cdr stream = failwith "Implement"
+  let cdr stream = 
+    match stream with
+    | Cons (hd, tl) -> tl ()
+    | Nil -> failwith "Empty"
 
-  let take n stream = failwith "Implement"
+  let rec take n stream = 
+    match stream with
+    | Cons (hd, tl) ->
+      if n = 0 then Nil
+      else Cons (hd, fun () -> take (n-1) (tl ()))
+    | Nil -> Nil
 
-  let fromList lst = failwith "Implement"
+  let rec fromList lst = 
+    match lst with
+    | hd :: tl -> Cons (hd, fun () -> fromList tl)
+    | [] -> Nil
 
